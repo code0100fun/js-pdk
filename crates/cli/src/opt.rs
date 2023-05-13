c948f88bde5e8d2583e57c18d5518f8491ebbcfd
Chase McCarthy <chase@code0100fun.com>
2023-05-12 22:19:29 -0500

    Fix bulk memory error

--- crates/cli/src/opt.rs
+++ crates/cli/src/opt.rs
@@ -23,6 +23,7 @@ impl<'a> Optimizer<'a> {
     pub fn write_optimized_wasm(self, dest: impl AsRef<Path>) -> Result<(), Error> {
         let mut wasm = Wizer::new()
             .allow_wasi(true)?
+            .wasm_bulk_memory(true)
             .inherit_stdio(true)
             .run(self.wasm)?;
 
